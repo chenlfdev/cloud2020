bfdc2e2e190507e76143cb23a000deba51f07df9
clf <1424275288@qq.com>
2020-07-16 00:55:48 +0800

    20200716

---
+++ cloud-consumer-order80/src/main/java/com/clf/springcloud/controller/OrderController.java
@@ -0,0 +1,29 @@
+package com.clf.springcloud.controller;
+
+import com.clf.springcloud.entities.CommonResult;
+import com.clf.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+        public static String PAYMENT_URL = "http://localhost:8001";
+    //public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
+    @RequestMapping("/payment/create")
+    public CommonResult<Payment> create(Payment payment){
+        return restTemplate.postForObject(PAYMENT_URL+"/create",payment, CommonResult.class);
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        return restTemplate.getForObject(PAYMENT_URL+"/get/"+id,CommonResult.class);
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/clf/springcloud/dao/PaymentDao.java
+++ cloud-provider-payment8001/src/main/java/com/clf/springcloud/dao/PaymentDao.java
@@ -2,7 +2,8 @@ package com.clf.springcloud.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.clf.springcloud.entities.Payment;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface PaymentDao extends BaseMapper<Payment> {
 }
